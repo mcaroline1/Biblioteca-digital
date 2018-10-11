9b734f1dcf50ac2640f065742e3566e09bb955f1
mcaroline1 <43241822+mcaroline1@users.noreply.github.com>
2018-10-11 10:56:54 -0300

    Adicionando o index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>8tavo batalhão</title>
+		
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+<body>
+
+	<img src="imgs/brasao-pmrn.jpg" alt="Brasão da PMRN" class="center">
+
+</body>
+</html>
